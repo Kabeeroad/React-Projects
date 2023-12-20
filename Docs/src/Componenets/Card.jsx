3a5d2a5ef96cc266c9b724158459d61a06b534c8
Kabeer <kabeeroad07@gmail.com>
2023-12-20 17:09:07 +0000

    Data randring isWorking

--- Docs/src/Componenets/Card.jsx
+++ Docs/src/Componenets/Card.jsx
@@ -6,10 +6,7 @@ const Card = () => {
   return (
     <div className=" relative  w-60 h-72 bg-zinc-900/90 rounded-[45px] text-white py-8 px-7 overflow-hidden">
       <FaRegFileLines />
-      <p className=" text-sm leading-tight mt-5 font-semibold ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        facere officia dicta!
-      </p>
+      <p className=" text-sm leading-tight mt-5 font-semibold "></p>
       <div className="footer absolute  bottom-0 w-full  left-0 ">
         <div className="flex justify-between items-center py-3 px-8 mb-2">
           <h5>.4mb</h5>
--- Docs/src/Componenets/Foreground.jsx
+++ Docs/src/Componenets/Foreground.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import Card from "./Card";
 
 const Foreground = () => {
+  const data = [
+    {
+      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+  ];
   return (
     <div className="fixed top-0 left-0 z-[3] w-full h-full ">
-      <Card />
+      {data.map(() => {
+        helo;
+      })}
     </div>
   );
 };
