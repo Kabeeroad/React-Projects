de4dc9e59b43464d4f7f3e109afb45478680cae9
Kabeer <kabeeroad07@gmail.com>
2023-12-20 12:31:11 +0000

    foreground Working Don\!

--- Docs/src/Componenets/Card.jsx
+++ Docs/src/Componenets/Card.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { FaRegBookmark, FaRegFileLines } from "react-icons/fa6";
 
 const Card = () => {
-  return <div className="w-60 h-72 bg-zinc-900/90 rounded-[20px]"></div>;
+  return (
+    <div className=" relative  w-60 h-72 bg-zinc-900/90 rounded-[20px] text-white p-5 overflow-hidden">
+      <FaRegFileLines />
+      <p className=" text-sm leading-tight mt-5 font-semibold ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        facere officia dicta!
+      </p>
+      <div className="footer absolute  bottom-0 bg-sky-200 w-full h-20 left-0 "></div>
+    </div>
+  );
 };
 
 export default Card;
