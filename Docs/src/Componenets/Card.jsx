5568646d9848358f82ad0f6b6461e564f7d7cfa7
Kabeer <kabeeroad07@gmail.com>
2023-12-21 04:34:58 +0000

    App-Data-Not-Showing

--- Docs/src/Componenets/Card.jsx
+++ Docs/src/Componenets/Card.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import { FaRegBookmark, FaRegFileLines } from "react-icons/fa6";
 import { MdFileDownload, MdOutlineFileDownload } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
 
-const Card = () => {
+const Card = ({ data }) => {
+  //destructuring data
   return (
-    <div className=" relative  w-60 h-72 bg-zinc-900/90 rounded-[45px] text-white py-8 px-7 overflow-hidden">
+    <div className=" relative flex-shrink-0  w-60 h-72 bg-zinc-900/90 rounded-[45px] text-white py-8 px-7 overflow-hidden">
       <FaRegFileLines />
-      <p className=" text-sm leading-tight mt-5 font-semibold "></p>
+      <p className=" text-sm leading-tight mt-5 font-semibold ">{data.desc}</p>
       <div className="footer absolute  bottom-0 w-full  left-0 ">
         <div className="flex justify-between items-center py-3 px-8 mb-2">
-          <h5>.4mb</h5>
+          <h5>{data.filesize}</h5>
           <span className=" w-7 h-7 bg-zinc-600 rounded-full items-center justify-center flex">
-            <MdOutlineFileDownload size=".9em" color="#fff" />
+            {data.close ? (
+              <IoMdClose />
+            ) : (
+              <MdOutlineFileDownload size=".9em" color="#fff" />
+            )}
           </span>
         </div>
-        <div className=" tag w-full py-4 bg-green-600 flex items-center justify-center">
-          <h3 className=" text-sm font-semibold">Download Now</h3>
-        </div>
+        {data.tag.isOpen && (
+          <div
+            className={`tag w-full py-4 bg-${data.tag.tagColor}-600 flex items-center justify-center`}
+          >
+            <h3 className=" text-sm font-semibold">{data.tag.tagTitle}</h3>
+          </div>
+        )}
       </div>
     </div>
   );
--- Docs/src/Componenets/Foreground.jsx
+++ Docs/src/Componenets/Foreground.jsx
@@ -9,12 +9,24 @@ const Foreground = () => {
       close: true,
       tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
     },
+    {
+      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: ".9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "yellow" },
+    },
+    {
+      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
   ];
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full ">
-      {data.map(() => {
-        helo;
-      })}
+    <div className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5 ">
+      {data.map((item, index) => (
+        <Card data={item} />
+      ))}
     </div>
   );
 };
