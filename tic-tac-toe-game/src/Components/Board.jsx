9765dc255de39a8d6764ee959b3deea4a4eb0b33
Kabeer <kabeeroad07@gmail.com>
2024-01-24 18:31:05 +0000

    tictac

--- tic-tac-toe-game/src/Components/Board.jsx
+++ tic-tac-toe-game/src/Components/Board.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-
+import "./Board.css";
 const Board = () => {
   return (
     <>
       <h1 className=" text-center">Hey👋</h1>
-      <tabl className=" ">
+      <tabl>
         <tr>
           <td>1</td>
           <td>2</td>
           <td>3</td>
         </tr>
         <tr>
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
         </tr>
         <tr>
-          <td>6</td>
-          <td>7</td>
-          <td>8</td>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
         </tr>
       </tabl>
     </>
