515ed909bb51cef1f8d4e17a9badbc28e7c818f7
Kabeer <kabeeroad07@gmail.com>
2024-01-26 17:54:43 +0000

    app completed

--- tic-tac-toe-game/src/Components/Board.jsx
+++ tic-tac-toe-game/src/Components/Board.jsx
@@ -2,26 +2,28 @@ import React, { useState } from "react";
 import "./Board.css";
 const Board = () => {
   const [board, setBoard] = useState(Array(9).fill(""));
-
+  const handelclick = () => {
+    console.log("cliked");
+  };
   return (
     <>
       <h1>Hello</h1>
       <table>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
+            <td color="handelclick">{board[0]}</td>
+            <td>{board[1]}</td>
+            <td>{board[2]}</td>
           </tr>
           <tr>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
+            <td>{board[3]}</td>
+            <td>{board[4]}</td>
+            <td>{board[5]}</td>
           </tr>
           <tr>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
+            <td>{board[6]}</td>
+            <td>{board[7]}</td>
+            <td>{board[8]}</td>
           </tr>
         </tbody>
       </table>
