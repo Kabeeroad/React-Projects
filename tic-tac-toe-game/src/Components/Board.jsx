63a2a74176c68d770b028dc69177d55e50049ec6
Kabeer <kabeeroad07@gmail.com>
2024-01-27 17:55:24 +0000

    app completed

--- tic-tac-toe-game/src/Components/Board.jsx
+++ tic-tac-toe-game/src/Components/Board.jsx
@@ -2,28 +2,38 @@ import React, { useState } from "react";
 import "./Board.css";
 const Board = () => {
   const [board, setBoard] = useState(Array(9).fill(""));
-  const handelclick = () => {
-    console.log("cliked");
+  const [currentplayer, setCurrentPlayer] = useState("x");
+  const click = (n) => {
+    let sequare = [...board];
+    sequare[n] = currentplayer;
+    setBoard(sequare);
+    if (currentplayer === "x") {
+      setCurrentPlayer("0");
+    } else {
+      setCurrentPlayer("x");
+    }
+    console.log("clicked", n);
   };
+
   return (
     <>
       <h1>Hello</h1>
       <table>
         <tbody>
           <tr>
-            <td color="handelclick">{board[0]}</td>
-            <td>{board[1]}</td>
-            <td>{board[2]}</td>
+            <td onClick={(n) => click(0)}>{board[0]}</td>
+            <td onClick={(n) => click(1)}>{board[1]}</td>
+            <td onClick={(n) => click(2)}>{board[2]}</td>
           </tr>
           <tr>
-            <td>{board[3]}</td>
-            <td>{board[4]}</td>
-            <td>{board[5]}</td>
+            <td onClick={(n) => click(3)}>{board[3]}</td>
+            <td onClick={(n) => click(4)}>{board[4]}</td>
+            <td onClick={(n) => click(5)}>{board[5]}</td>
           </tr>
           <tr>
-            <td>{board[6]}</td>
-            <td>{board[7]}</td>
-            <td>{board[8]}</td>
+            <td onClick={(n) => click(6)}>{board[6]}</td>
+            <td onClick={(n) => click(7)}>{board[7]}</td>
+            <td onClick={(n) => click(8)}>{board[8]}</td>
           </tr>
         </tbody>
       </table>
