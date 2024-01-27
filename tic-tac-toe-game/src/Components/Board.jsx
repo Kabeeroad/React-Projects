97abdf35bad73bbb42446f931812d6f02dce80ed
Kabeer <kabeeroad07@gmail.com>
2024-01-27 18:02:57 +0000

    app completed

--- tic-tac-toe-game/src/Components/Board.jsx
+++ tic-tac-toe-game/src/Components/Board.jsx
@@ -14,7 +14,13 @@ const Board = () => {
     }
     console.log("clicked", n);
   };
-
+  function checkWinner(board) {
+    const winingConditions = [
+      [0, 1, 2][(3, 4, 5)][(6, 7, 8)][(0, 3, 6)][(1, 4, 7)][(2, 5, 8)][
+        (0, 4, 8)
+      ][(2, 4, 6)],
+    ];
+  }
   return (
     <>
       <h1>Hello</h1>
