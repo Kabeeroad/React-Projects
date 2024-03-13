37c5adbdda5276540a70b663ec6899694c229325
Kabeer <kabeeroad07@gmail.com>
2024-03-13 22:10:48 +0500

    adding new taske

---
+++ class4/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "./Components/Nav/Nav";
+import Footer from "./Components/Footer/Footer";
+import Card from "./Components/Cards/Card";
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      {/* <Card /> */}
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ class4/src/Components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return <div className="footer">Copyrigt &copy; DuckCoder | All right resrved</div>;
+};
+
+export default Footer;
