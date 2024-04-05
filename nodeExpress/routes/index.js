c0aef10481e2c0a29ed88e95e2cf571b202f459a
Kabeer <kabeeroad07@gmail.com>
2024-04-05 10:37:03 +0500

    cokies and session

--- nodeExpress/routes/index.js
+++ nodeExpress/routes/index.js
@@ -1,9 +1,26 @@
 var express = require("express");
+const session = require("express-session");
 var router = express.Router();
+// const userModel = require("./users");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
+  req.session.ban = true;
+
   res.render("index");
 });
 
+router.get("/ban", function (req, res) {
+  if (req.session.ban === true) {
+    res.send("your bande");
+  } else {
+    res.send("not band");
+  }
+});
+
+router.get("/remove", function (req, res) {
+  req.session.destroy(function (err) {
+    if (err) throw err;
+    res.send("ban removed");
+  });
+});
 module.exports = router;
